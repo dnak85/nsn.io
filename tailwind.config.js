1ecb4a48d351b34b8b6a8e39978d8d6c786c6760
dnak <dnak8519@gmail.com>
2023-02-10 15:45:43 +0900

    dist update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,15 @@
 module.exports = {
     content: ['./src/**/*.{html,js}'],
     theme: {
-        extend: {},
+        extend: {
+            boxShadow: {
+                cs1: '0px 15px 15px rgba(0, 0, 0, 0.2)', //custom 1
+            },
+        },
         fontFamily: {
             ptnd: ['Pretendard'],
             dm: ['DM Sans'],
+            gm: ['GmarketSans'],
         },
     },
     plugins: [],
