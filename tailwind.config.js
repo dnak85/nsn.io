bb278bf09f8d31fd8347df800aae6496da50e406
dnak <nryn6563@kakao.com>
2023-02-13 16:22:13 +0900

    no message

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,16 @@ module.exports = {
         extend: {
             boxShadow: {
                 cs1: '0px 15px 15px rgba(0, 0, 0, 0.2)', //custom 1
+                cs2: '0px 0px 0px 2px rgba(255, 255, 255, 0.8)', //custom2
             },
         },
         fontFamily: {
             ptnd: ['Pretendard'],
             dm: ['DM Sans'],
             gm: ['GmarketSans'],
+        },
+        screens : {
+            'xsm' : '280px',
         },
     },
     plugins: [],
