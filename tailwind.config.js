c4efb1dac094c40271c7f10f21d2828bacd22046
dnak85 <nryn6563@kakao.com>
2023-05-22 15:15:39 +0900

    0518 반환 페이지 수정사항

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
         cs2: "0px 0px 0px 2px rgba(255, 255, 255, 0.8)", //custom2
         cs3: "0px 00px 16px rgba(0,0,0, 0.25)", // custom3
       },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
     },
     fontFamily: {
       ptnd: ["Pretendard"],
