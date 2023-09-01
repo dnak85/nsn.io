4d1bf8b25b4fa92ff17737bbfba662309e61d34d
dnak85 <nryn6563@kakao.com>
2023-09-01 09:04:04 +0900

    no message

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
       boxShadow: {
         cs1: "0px 15px 15px rgba(0, 0, 0, 0.2)", //custom 1
         cs2: "0px 0px 0px 2px rgba(255, 255, 255, 0.8)", //custom2
-        cs3: "0px 00px 16px rgba(0,0,0, 0.25)", // custom3
+        cs3: "0px 0px 0px 16px rgba(0,0,0, 0.25)", // custom3
+        cs4: "0px 8px 8px 0px rgba(57, 66, 106, 0.2)", // custom4
       },
       animation: {
         "spin-slow": "spin 3s linear infinite",
@@ -23,9 +24,6 @@ module.exports = {
     screens: {
       xsm: { raw: "(max-width: 280px)" },
       lg: "950px",
-    },
-    animation: {
-      "spin-slow": "spin 3s linear infinite",
     },
   },
   plugins: [],
