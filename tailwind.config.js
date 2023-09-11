c5e02bfce2e61294c066c1688905bc7c3c176f53
dnak85 <nryn6563@kakao.com>
2023-09-11 10:11:29 +0900

    노쇼노개편사항 반환부분 포함

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         cs1: "0px 15px 15px rgba(0, 0, 0, 0.2)", //custom 1
         cs2: "0px 0px 0px 2px rgba(255, 255, 255, 0.8)", //custom2
         cs3: "0px 0px 0px 16px rgba(0,0,0, 0.25)", // custom3
-        cs4: "0px 8px 8px 0px rgba(57, 66, 106, 0.2)", // custom4
+        cs4: "0px 8px 8px 0px #CCBAF4", // custom4
       },
       animation: {
         "spin-slow": "spin 3s linear infinite",
